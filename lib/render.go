04fbbfd5c5ed1f1b6c845edfe66642ea3cc69bdc
Kim Eunji <51512016+Eundongdong@users.noreply.github.com>
2024-06-25 17:50:21 +0900

    Update render.go

--- lib/render.go
+++ lib/render.go
@@ -52,7 +52,7 @@ func (user User) Render() (string, error) {
 	buf.WriteString(
 		fmt.Sprintf("%s     %s\n%s     %s\n",
 			renderField("✅ Solved: ", user.SolvedCount, half),
-			renderField("💠 Class: ", user.Class, half)
+			renderField("💠 Class: ", user.Class, half),
 		),
 	)
 
